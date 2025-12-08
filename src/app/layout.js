965d341820528a49c8a7210be35a82518c4ed575
akshar <aamuti64@gmail.com>
2025-12-08 23:14:53 +0600

    feat: add initial root layout with header, footer, global CSS, and favicons

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,12 @@ import "@/app/globals.css";
 
 export const metadata = {
   icons: {
-    icon: '/images/AAMlogo.png',
+    icon: [
+      { url: '/favicon_io/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon_io/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon_io/favicon.ico', sizes: 'any' },
+    ],
+    apple: '/favicon_io/apple-touch-icon.png',
   },
 };
 
